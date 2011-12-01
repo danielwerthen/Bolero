14e7c085fc9ae9c14e5c1a16f9d1caf31deafd5e
Daniel Werthén <danielwerthen@gmail.com>
2011-12-01 14:45:47 +0000

    Upgraded the bolerodb script and improved the channeling between client and server

--- socket.js
+++ socket.js
@@ -4,12 +4,23 @@ var connect = require('connect');
 var sio = require('socket.io');
 var app = express.createServer();
 var io = sio.listen(app);
-var dbio = require('./dbio');
+var dbio = require('./dbio-v2');
 
 app.use(connect.static(__dirname + '/public'));
 
-app.get('/', function(req, res) {
-  res.end();
+app.get('/', function (req, res, next) {
+  dbio.getDocs('thoughts', function (err, thoughts) {
+    if (err !== null) {
+      res.end();
+      return;
+    }
+    if (thoughts === undefined) {
+      res.end();
+      return;
+    }
+    res.write(JSON.stringify({ thoughts: thoughts }));
+    res.end();
+  }, {userId: "4ed38cda7617b7680a000001" });
 });
 
 if (process.env.PORT)
@@ -19,44 +30,73 @@ else
 
 io.set('log level', 1);                    // reduce logging
 
-
-var thinkers = io
-.of('/thoughts')
-.on('connection', function(socket) {
-    socket.emit('handshake', {
-      authorized: (socket.auth === true) ? true : false
-    });
-    socket.on('handshake', function(data) {
-      if (data.username && data.password) {
-        dbio.authenticate(data.username, data.password, function(result) {
-          socket.set('auth', result, function() {
-            socket.emit('handshake', {
-              authorized: result
+dbio(function(err, db) {
+  if (err || db === null) {
+    console.log('failed to open BoleroDb : ' + db);
+    return;
+  }
+  var thinkers = io
+    .of('/thoughts')
+    .on('connection', function(socket) {
+        socket.emit('handshake', {
+          authorized: (socket.auth === true) ? true : false
+        });
+        socket.on('handshake', function(data) {
+          if (data.username && data.password) {
+            db.toArray('users', {username: data.username, password: data.password}, function(err, user) {
+              if (err || user.length == 0) {
+                socket.emit('handshake', {
+                  authorized: false
+                });
+              }
+              else {
+                socket.set('user', user[0], function() {
+                  socket.emit('handshake', {
+                    authorized: true
+                  });
+                });
+              }
             });
+          }
+          else {
+            socket.get('user', function(err, user) {
+              socket.emit('handshake', {
+                authorized: user === null ? false : true
+              });
+            });
+          }
+        });
+        
+        socket.on('getthoughts', function(filter) {
+          socket.get('user', function(err, user) {
+            if (!err) {
+              var query = {};
+              var abort = false;
+              if (filter) {
+                if (filter.userId !== undefined) {
+                  if (typeof filter.userId == 'string')
+                    query.userId = filter.userId;
+                  else if (filter.userId.toString !== undefined)
+                    query.userId = filter.userId.toString();
+                  else
+                    abort = true;
+                }
+                else
+                  query.userId = user._id.toString();
+              }
+              if (!abort) {
+                db.foreach('thoughts', { userId: user._id.toString() }, function (err, thought) {
+                  if (!err && thought !== null)
+                    socket.emit('thought', thought);
+                });
+              }
+            }
+            else {
+              socket.emit('getthoughts', {
+                authorized: false
+              });
+            }
           });
         });
-      }
-      else {
-        socket.get('auth', function(err, auth) {
-          socket.emit('handshake', {
-            authorized: err ? false : auth
-          });
-        });
-      }
-    });
-    
-    socket.on('getthoughts', function(data) {
-      socket.get('auth', function(err, auth) {
-        if (auth === true) {
-          
-        }
-        else {
-          socket.emit('getthoughts', {
-            authorized: false
-          });
-        }
       });
-    });
-  });
-
-
+});
